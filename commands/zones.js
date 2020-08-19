10e263479463392527234e0ecea54be1d3daf05c
Benjamin Young <byoung2@wiley.com>
2020-08-19 11:13:37 -0400

    Add Cloudflare to configDir compare to zones

--- commands/zones.js
+++ commands/zones.js
@@ -1,9 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
 const axios = require('axios');
 const chalk = require('chalk');
 const level = require('level');
 
 // foundational HTTP setup to Cloudflare's API
 axios.defaults.baseURL = 'https://api.cloudflare.com/client/v4';
+
+// check if local redirect description exists
+function findDescription(domain, dir) {
+  // create file path for both available formats
+  let redir_filepath_json = path.join(process.cwd(), dir, `${domain}.json`);
+  let redir_filepath_yaml = path.join(process.cwd(), dir, `${domain}.yaml`);
+  // check if file exists
+  if (fs.existsSync(redir_filepath_json)) {
+    return path.relative(process.cwd(), redir_filepath_json);
+  }
+  if (fs.existsSync(redir_filepath_yaml)) {
+    return path.relative(process.cwd(), redir_filepath_yaml);
+  }
+  return false;
+}
 
 /**
  * Lists available Zones/Sites in Cloudflare
@@ -15,6 +33,16 @@ exports.builder = (yargs) => {
     describe: `API (Bearer) token for the Cloudflare API (WR_CLOUDFLARE_TOKEN)`,
     demandOption: true,
     type: 'string'
+  }).option('configDir', {
+    type: 'string',
+    describe: 'directory containing the redirect descriptions (WR_CONFIG_DIR)',
+    default: '.',
+    coerce(v) {
+      return {
+        name: v,
+        contents: fs.readdirSync(v, 'utf8')
+      };
+    }
   });
 };
 exports.handler = (argv) => {
@@ -26,14 +54,33 @@ exports.handler = (argv) => {
 
       console.log(`${chalk.bold(resp.data.result.length)} Zones:`);
       // loop through the returned zones and store a domain => id mapping
+      let zone_names = [];
       resp.data.result.forEach((zone) => {
+        zone_names.push(zone.name);
         console.log(`
   ${chalk.bold(zone.name)} - ${zone.id} in ${zone.account.name}
   ${zone.status === 'active' ? chalk.green('✓') : chalk.blue('🕓')} ${chalk.green(zone.plan.name)} - ${zone.meta.page_rule_quota} Page Rules available.`);
+        let description_file = findDescription(zone.name, argv.configDir.name);
+        if (description_file) {
+          console.log(chalk.keyword('purple')(`  Redirect description exists: ${description_file}`));
+        }
         db.put(zone.name, zone.id)
           .catch(console.error);
       });
       db.close();
+
+      // list any redirect descriptions available which do not appear in Cloudflare
+      let missing = argv.configDir.contents.filter((filename) => {
+        return filename[0] !== '.'
+          && zone_names.indexOf(filename.substr(0, filename.length-5)) === -1;
+      });
+
+      if (missing.length > 0) {
+        console.log(`\nThe following ${chalk.bold(missing.length)} domains are not yet in Cloudflare:`);
+        missing.forEach((li) => {
+          console.log(` - ${li}`);
+        });
+      }
     })
     .catch((err) => {
       console.error(err);
