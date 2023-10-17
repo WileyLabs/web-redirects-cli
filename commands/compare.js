27cd452c2d98bc7f40312cdf979231bc20d4f336
Alun 'Will' Williams <alun@alunw.com>
2023-10-17 16:03:22 +0100

    Factored out last Cloudflare API call from commands

--- commands/compare.js
+++ commands/compare.js
@@ -20,7 +20,13 @@ import {
   convertRedirectToPageRule,
   outputPageRulesAsText
 } from '../lib/shared.js';
-import { getZoneById, getZonePageRulesById } from '../lib/cloudflare.js';
+import { 
+  getZoneById,
+  getZonePageRulesById,
+  createPageRule,
+  deletePageRule,
+  updatePageRule
+} from '../lib/cloudflare.js';
 
 // foundational HTTP setup to Cloudflare's API
 axios.defaults.baseURL = 'https://api.cloudflare.com/client/v4';
@@ -156,47 +162,31 @@ const handler = (argv) => {
                     // TODO: switch this to use Promise.all?
                     Object.keys(modifications).forEach((key) => {
                       const mod = modifications[key];
-                      // post doesn't need an ID
-                      const url = modifications[key].method === 'post'
-                        ? `/zones/${zone_id}/pagerules`
-                        : `/zones/${zone_id}/pagerules/${key}`;
-                      axios[mod.method](url,
-                        // delete doesn't need a body
-                        mod.method === 'delete' ? {} : mod.pagerule)
-                        .then((resp) => {
-                          if (resp.data.success) {
-                            switch (mod.method) {
-                              case 'delete':
-                                console.log(`Page rule ${key} has been removed.`);
-                                break;
-                              case 'post':
-                                console.log(`The following page rule was created and enabled:`);
-                                outputPageRulesAsText([resp.data.result]);
-                                break;
-                              case 'put':
-                                console.log(`Page rule ${key} has been updated:`);
-                                outputPageRulesAsText([resp.data.result]);
-                                break;
-                              default:
-                                break;
-                            }
-                          }
-                        })
-                        .catch((err) => {
-                          // TODO: handle errors better... >_<
-                          if ('response' in err
-                              && 'status' in err.response) {
-                            if (err.response.status === 403) {
-                              error(`The API token needs the ${chalk.bold('#zone.edit')} permissions enabled.`);
-                            } else if (err.response.status === 400) {
-                              console.dir(err.response.data);
-                            } else {
-                              console.error(err);
-                            }
-                          } else {
-                            console.error(err);
-                          }
-                        });
+                      // TODO
+                      switch (mod.method) {
+                        case 'delete':
+                          deletePageRule(zone_id, key)
+                            .then(console.log(`Page rule ${key} has been removed.`));
+                          break;
+                        case 'post':
+                          createPageRule(zone_id, mod.pagerule)
+                            .then((response) => {
+                              console.log('The following page rule was created and enabled:');
+                              outputPageRulesAsText([response.data.result]);
+                            });
+                          break;
+                        case 'put':
+                          updatePageRule(zone_id, key, mod.pagerule)
+                            .then((response) => {
+                              console.log(`Page rule ${key} has been updated:`);
+                              outputPageRulesAsText([response.data.result]);
+                            });
+                          break;
+                        default:
+                          console.error(chalk.yellow('Unhandled pagerule method:'));
+                          console.error(mod);
+                          break;
+                      }
                     });
                     // TODO: tell the user to tweak permissions if it fails
                   }
