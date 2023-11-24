29cf0c381cf821c644f2c7d367b87790508fec58
Alun 'Will' Williams <alun@alunw.com>
2023-11-24 16:00:27 +0000

    Initial check of settings

--- commands/sync.js
+++ commands/sync.js
@@ -2,13 +2,15 @@
  * @copyright 2023 John Wiley & Sons, Inc.
  * @license MIT
  */
-import * as path from 'node:path';
+import lodash from 'lodash';
 import {
+  green, blue, red, orange, purple, lightblue,
   getLocalYamlSettings,
   getLocalYamlZones
 } from '../lib/sync-shared.js';
 import {
-  getZonesByAccount
+  getZonesByAccount,
+  getZoneSettingsById
 } from '../lib/cloudflare.js';
 
 const command = 'sync';
@@ -37,24 +39,52 @@ const builder = (yargs) => {
     });
 };
 
-const handler = async (argv) => {
-  // console.debug('argv', argv);
+let localZoneSettings;
 
-  // load local config to cache (fail on error - e.g. missing params)
-  const localZoneSettings = await getLocalYamlSettings(argv.configDir);
+const { isEqual } = lodash;
 
-  // fetch list of all zones defined in yaml configuration
-  const yamlZones = await getLocalYamlZones(argv.configDir);
+const insertValue = (map, key, value) => {
+  const currentData = map.get(key);
+  if (currentData) {
+    map.set(key, { ...currentData, ...value });
+  } else {
+    map.set(key, value);
+  }
+  return null;
+};
 
-  // load remote config to cache (fail on error - e.g. missing params/credentials)
-  const cfZones = await getZonesByAccount(argv.accountId);
+const areZoneSettingsValid = async (zoneId) => {
+  const cfSettings = await getZoneSettingsById(zoneId);
+  let valid = true;
+  Object.keys(localZoneSettings).forEach((setting) => {
+    const localValue = localZoneSettings[setting];
+    const remoteValue = cfSettings.filter((prop) => prop.id === setting);
+    if (!isEqual(localValue, remoteValue[0].value)) {
+      valid = false;
+      console.log(`${setting} | ${JSON.stringify(localValue)} | ${JSON.stringify(remoteValue[0].value)}`);
+    }
+  });
+  return valid;
+};
 
-  const yamlZoneNames = yamlZones.map((yaml) => yaml.zone);
-  const cfZoneNames = cfZones.map((zone) => zone.name);
-  const mergedNames = [...new Set([...yamlZoneNames, ...cfZoneNames])];
-  console.log(mergedNames);
+const processZone = async (key, data) => {
 
-  // need to load other data for pluggable modules? do it now.
+  // 1. is zone in yaml, cloudflare or both?
+  if (data.yaml && data.cloudflare) {
+    const result = await areZoneSettingsValid(data.cloudflare.id);
+    if (result) {
+      console.log(green(`${key}`));
+    } else {
+      console.log(orange(`${key} [incorrect settings]`));
+    }
+  } else if (data.yaml) {
+    console.log(blue(`${key} [Not in Cloudflare yet!]`));
+  } else if (data.cloudflare) {
+    console.log(purple(`${key} [No YAML defined!]`));
+  } else {
+    console.log(red(`ERROR: ${key}`));
+  }
+
 
   // compare data (pluggable modules - interface to be defined)
 
@@ -63,6 +93,26 @@ const handler = async (argv) => {
   // update cloudflare (pluggable modules - interface to be defined)
 
   // opt. persist remote config (pluggable modules - interface to be defined)
+
+};
+
+const handler = async (argv) => {
+  // console.debug('argv', argv);
+
+  // load local config to cache (fail on error - e.g. missing params)
+  localZoneSettings = await getLocalYamlSettings(argv.configDir);
+
+  const zoneData = new Map();
+
+  // fetch list of all zones defined in yaml configuration
+  const yamlZones = await getLocalYamlZones(argv.configDir);
+  yamlZones.map((data) => insertValue(zoneData, data.zone, { yaml: data }));
+
+  // load remote config to cache (fail on error - e.g. missing params/credentials)
+  const cfZones = await getZonesByAccount(argv.accountId);
+  cfZones.map((data) => insertValue(zoneData, data.name, { cloudflare: data }));
+
+  Array.from(zoneData.keys()).forEach((zone) => processZone(zone, zoneData.get(zone)));
 };
 
 export {
