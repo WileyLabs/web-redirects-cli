43efd4ee00d40880570c69366466e0fefe99d6f7
Alun 'Will' Williams <alun@alunw.com>
2024-11-28 16:30:34 +0000

    Update describe command

--- commands/describe.js
+++ commands/describe.js
@@ -1,8 +1,8 @@
 /* eslint no-console: "off" */
 import * as fs from 'node:fs';
 import * as path from 'node:path';
-import chalk from 'chalk';
 import * as YAML from 'js-yaml';
+import { green } from '../lib/shared.js';
 
 /**
  * Describe a redirect as a YAML file
@@ -22,14 +22,14 @@ const handler = (argv) => {
   const { configDir, domain, destination } = argv;
   const redirect = YAML.dump({
     name: domain,
-    redirects: [{ from: '/*', to: destination }]
+    redirects: [{ from: '^/(.*)', to: destination }]
   });
   const filepath = path.join(configDir.name, `${domain}.yaml`);
 
   fs.writeFile(filepath, redirect, (err) => {
     if (err) throw err;
 
-    console.log(chalk.green(`The following redirect description was saved into ${filepath}:`));
+    console.log(green(`The following redirect description was saved into ${filepath}:`));
     console.log(redirect);
   });
 };
